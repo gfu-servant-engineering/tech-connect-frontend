492722391ca7e82e54100490af7518e2232b9184
Kaylee Hafferkamp <khafferkamp16@georgefox.edu>
2019-04-17 00:06:20 -0700

    Updated the profile page, adding images and tabs to make the page a little nicer

--- src/templates/specific-profile.js
+++ src/templates/specific-profile.js
@@ -2,72 +2,95 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout.js'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage.js'
+import Tabs from '../components/Tabs/Tabs.js'
+import { FaCheck, FaUser, FaList, FaExclamationTriangle } from 'react-icons/fa'
+import LinesEllipsis from 'react-lines-ellipsis'
 
 const ProfileTemplate = ({ data }) => (
     <Layout>
       <section className="section">
-        <div className="columns is-centered">
-          <div className="column is-5">
-          </div>
-          <div className="column is-5">
+        <div className="columns is-centered is-multiline">
+            <div className="column is-full-mobile is-two-thirds-tablet is-half-desktop" style={{maxWidth:'400px'}}>
+                <PreviewCompatibleImage imageInfo={data.strapiProfile.profile_image}/>
+            </div>
+          <div className="column is-full-mobile is-two-thirds-tablet is-half-desktop">
               <div style={{display:'inline', padding: '5%'}}>
                 <h1 className="has-text-weight-bold is-size-2">{data.strapiProfile.profile_name}</h1>
-                <hr style={{
-                    color:'#1C2833',
-                    backgroundColor: '#1C2833',
-                    height: 2
-                  }}/>
-                <br/>
-                <br/>
-				<br/>
-                <br/>
+                <div className="is-italic">
+                {data.strapiProfile.profile_location}
+                </div>
+                <hr className="horizontal-rule" />
+                <div className="column is-12">
+                    {data.strapiProfile.profile_bio}
+                </div>
               </div>
             </div>
-        </div>
-        <div className="columns is-centered is-multiline">
-          <div className="column is-10">
-            <br/>
-            <h4 className="has-text-weight-bold is-size-3">My Experiences</h4>
-              <div>{data.strapiProfile.experiences.map(experience => (
-				  <div>
-				  <br/>
-					<div className="has-text-weight-bold is-size-5">
-					  {experience.experience_title}
-					  <br />
-					</div>
-					<div>
-					  {experience.experience_description}
-					</div>
-				  </div>
-				))}</div>
+        </div> <br/>
+
+        <div>
+        <Tabs>
+          <div label="Skills" icon="FaCheck">
+            <center>{data.strapiProfile.skills.map(skill => (
+                <div>
+                <br/>
+                <div>
+                  {skill.skill_name}
+                  <br />
+                </div>
+                </div>
+            ))}</center>
+            <br /><br />
           </div>
-          <div className="column is-10">
-              <br/>
-              <h4 className="has-text-weight-bold is-size-3">My Skills</h4>
-              <p>{data.strapiProfile.skills.map(skill => (
-				  <div>
-				  <br/>
-					<div>
-					  {skill.skill_name}
-					  <br />
-					</div>
-				  </div>
-				))}</p>
+
+
+          <div label="Experience" icon="FaUser">
+
+              <div className="columns is-multiline is-centered">
+                {data.strapiProfile.experiences.map(experience => (
+                    <div className="column is-12">
+                        <div className="has-text-weight-bold is-size-5">
+                            {experience.experience_title} <br />
+                        </div>
+                        <div>
+                            {experience.experience_description}
+                        </div>
+                    </div>
+                ))}
+              </div> <br /><br />
           </div>
-          <div className="column is-10">
-              <br/>
-              <h4 className="has-text-weight-bold is-size-3">Projects I have worked on</h4>
-              <p>{data.strapiProfile.projects.map(project => (
-				  <div>
-				  <br/>
-					<div>
-					  <Link to={`/Project_${project.id}`}>{project.project_name}</Link>
-					  <br />
-					</div>
-				  </div>
-				))}</p>
+
+
+          <div label="Endorsements" icon="FaList">
+            <center>
+                <span class="icon is-medium"><FaExclamationTriangle size={"2em"}/></span>
+            </center>
+            <center>
+             This user does not have any endorsements at this time.
+             </center>
+            <br /><br />
           </div>
-        </div>
+
+          <div label="Projects" icon="FaPagelines">
+              <div className="columns is-8 is-multiline is-centered">
+                {data.strapiProfile.projects.map(project => (
+                    <div className="column is-8- mobile is-10-tablet is-4-desktop is-4-widescreen">
+                    <Link to={`/Project_${project.id}`}><PreviewCompatibleImage imageInfo={project.project_image} /></Link>
+                    <h3 className="has-text-weight-bold is-size-5">
+                      <Link style={{display:'inline-block', color: '#1C2833'}} to={`/Project_${project.id}`}>{project.project_name}</Link>
+                    </h3>
+                    <Link style={{color: '#1C2833'}} to={`/Project_${project.id}`}><LinesEllipsis
+                      text={project.project_description}
+                      maxLine='2'
+                      ellipsis='...'
+                      trimRight
+                      basedOn='letters'
+                      /></Link>
+                    </div>
+                ))}
+              </div>
+          </div>
+        </Tabs>
+      </div>
       </section>
     </Layout>
 )
@@ -78,6 +101,15 @@ export const pageQuery = graphql`
   query ProfileTemplate ($id: String!) {
     strapiProfile(id: {eq: $id}) {
       profile_name
+      profile_bio
+      profile_location
+      profile_image {
+         childImageSharp {
+            fluid(maxHeight:400, maxWidth:400, quality:90, toFormat:JPG) {
+               ...GatsbyImageSharpFluid
+            }
+          }
+        }
       skills {
 		  skill_name
 	  }
@@ -88,7 +120,16 @@ export const pageQuery = graphql`
 	  projects {
 		  id
 		  project_name
-	  }
-     }
+      project_image {
+         childImageSharp {
+            fluid(maxHeight:200, maxWidth:400, quality:90, toFormat:JPG) {
+               ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        project_description
+        profiles
+	    }
+    }
   }
   `
