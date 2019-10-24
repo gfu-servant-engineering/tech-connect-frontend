344fe4d4403ff8c2c7bc98f658173eb0e9cfa6f3
Chris <soundguy66@gmail.com>
2019-10-23 22:31:45 -0700

    Following instructions for adding auth0 support to gatsby. Still needs a login/logout button...

--- src/pages/create-project.js
+++ src/pages/create-project.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import Layout from '../components/Layout.js'
 import ProjectForm from '../components/ProjectForm.js'
+import { login, isAuthenticated, getProfile } from "../utils/auth"
 
-const ProjectFormTemplate = ({ data }) => (
-  <Layout>
-    <section>
-      <div class="hero">
-        <div class="hero-body">
-          <div class="container has-text-centered">
-            <h1 class="title" className="has-text-weight-bold is-size-1" style={{color: '#1C2833'}}>
-              Create a Project
-            </h1>
-            <h2 class="subtitle">
-              Ensure your projects needs are fulfilled by answering a couple questions.
-            </h2>
+
+const ProjectFormTemplate = ({ data }) => {
+  if (!isAuthenticated()) {
+    login()
+    return <p>Redirecting to login...</p>
+  }
+
+  const user = getProfile()
+  return (
+    <Layout>
+      <section>
+        <div class="hero">
+          <div class="hero-body">
+            <div class="container has-text-centered">
+              <h1 class="title" className="has-text-weight-bold is-size-1" style={{color: '#1C2833'}}>
+                Create a Project
+              </h1>
+              <h2 class="subtitle">
+                Ensure your projects needs are fulfilled by answering a couple questions.
+              </h2>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-    <ProjectForm />
-  </Layout>
-)
+      </section>
+      <ProjectForm />
+    </Layout>
+  )
+}
 
 export default ProjectFormTemplate
