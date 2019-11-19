6ec551d9236222aea2f892bd57ed2696f27ecf0f
RanDumSocks <justinchance@randumsocks.com>
2019-11-18 22:43:01 -0800

    Added 'no results found' text

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -53,6 +53,7 @@ const SearchResults = ({data, query, results}) => {
             </div>
           )
         })}
+        {!!query ? (<h1 className="is-primary">No Results Found For "{query}"</h1>) : (<p></p>)}
       </div>
     </section>
   )
