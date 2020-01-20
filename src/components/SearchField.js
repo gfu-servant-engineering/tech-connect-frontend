4ca5989b6ed26e3150c737e33f125fc1f2515a60
RanDumSocks <justinchance@randumsocks.com>
2020-01-20 12:11:04 -0800

    Added auto search
    Search page now waits one second of idle time after updating search field then automatically redirects user to the search results. Loading visual present while timeout is active.

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { navigate } from 'gatsby';
 
 var queryText
+var timeout
+
+function SubmitTimeout() {
+  if (!!timeout) {
+    clearTimeout(timeout)
+  } else {
+    var searchButton = document.getElementById("searchButton")
+    searchButton.classList.add("is-loading")
+  }
+  timeout = setTimeout(Submit, 1000)
+}
 
 function Submit() {
   navigate(`/project-search?keywords=${encodeURIComponent(queryText)}`)
+  var searchButton = document.getElementById("searchButton")
+  searchButton.classList.remove("is-loading")
+  timeout = undefined
 }
 
 const SearchForm = ({query}) => {
@@ -17,11 +31,11 @@ const SearchForm = ({query}) => {
           name="search"
           placeholder="Search Here"
           onKeyPress={(e) => {if (e.key === 'Enter') Submit()}}
-          onChange={(e) => {queryText = e.target.value}}
+          onChange={(e) => {queryText = e.target.value; SubmitTimeout()}}
         />
       </div>
       <div className="control">
-        <button onClick={() => Submit()} className="button is-link">Submit</button>
+        <button id="searchButton" onClick={() => Submit()} className="button is-link">Search</button>
       </div>
     </div>
   )
