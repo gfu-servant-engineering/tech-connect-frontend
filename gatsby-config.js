178b76300ba5937b20b0e062c531f159fdd108b5
RanDumSocks <justinchance@randumsocks.com>
2020-03-06 11:40:04 -0800

    Using environment variables for API URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
-        //apiURL: 'http://techconnect-api.ddns.net:1337',
+        apiURL: process.env.GATSBY_STRAPI_HOST,
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'project',
           'blogpage',
