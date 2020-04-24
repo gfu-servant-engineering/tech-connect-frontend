664c618406bd9c9b73f6f7b0b1aa8f3ca4adfad6
dougas19 <42753181+dougas19@users.noreply.github.com>
2020-04-24 13:49:50 -0700

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,6 @@ const _ = require('lodash')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 const { fmImagesToRelative } = require('gatsby-remark-relative-images')
-const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 
 
 const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
@@ -119,8 +118,8 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === "build-html") {
     /*
      * During the build step, `auth0-js` will break because it relies on
-     * browser-specific APIs. Fortunately, we don’t need it during the build.
-     * Using Webpack’s null loader, we’re able to effectively ignore `auth0-js`
+     * browser-specific APIs. Fortunately, we donâ€™t need it during the build.
+     * Using Webpackâ€™s null loader, weâ€™re able to effectively ignore `auth0-js`
      * during the build. (See `src/utils/auth.js` to see how we prevent this
      * from breaking the app.)
      */
